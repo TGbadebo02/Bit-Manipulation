241e1b5564fd0d0f84a44ea52c7d1e236aa44ab0
TGbadebo02 <TJ.gbadebo02@gmail.com>
2022-08-17 23:35:53 +0100

    Bit-Manipulation

--- src/Main.java
+++ src/Main.java
@@ -15,15 +15,16 @@ public class Main {
 
 
     public static int convertDecIMAL(int input, int base){
-        int answer=0;
-        int multiplier =1;
-        String symbols = "0123456789ABCDEF";
-        while(input!=0)
-        {
+        int answer=0;//this variable is created to store the result
+        int multiplier =1;//the multipying variable
+        String symbols = "0123456789ABCDEF";//these are the symbols that are used for the bit maipulation/
+        while(input!=0)// a while loop, to loop the the program until the condition is no longer met
+        {  // here is the overall calculation of the bit manipulation.
             answer = (input%10) * multiplier + answer;
             input=input/10;
             multiplier = multiplier * base;
         }
+        //the return statement for the answer at the end of the loop
         return answer;
     }
 }
